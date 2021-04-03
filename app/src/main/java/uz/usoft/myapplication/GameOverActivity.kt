57fe30b7691cacd68f87dbaeb549796d225548a0
Sharapat Kalabaev <kalabaevshj@gmail.com>
2021-04-03 19:38:05 +0500

    finish

--- app/src/main/java/uz/usoft/myapplication/GameOverActivity.kt
+++ app/src/main/java/uz/usoft/myapplication/GameOverActivity.kt
@@ -8,21 +8,21 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_game_over.*
 
 class GameOverActivity : AppCompatActivity() {
-    private lateinit var preferences: SharedPreferences
+
+    private var currentResult = 0
     private var maxScore = 0
-    private var score = 0
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
         preferences = getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE)
-        score = intent.getIntExtra("result", 0)
-        maxScore = preferences.getInt("MaxScore", 0)
-        tvMaxScore.text = maxScore.toString()
-        if (maxScore < score) {
-            Toast.makeText(this, "Urra, you have the best result", Toast.LENGTH_LONG).show()
-            preferences.edit().putInt("MaxScore", score).apply()
+        maxScore = preferences.getInt("max_score", 0)
+        currentResult = intent.getIntExtra("result", 0)
+        tvResult.text = "Your score is $currentResult"
+        if (currentResult > maxScore) {
+            preferences.edit().putInt("max_score", currentResult).apply()
         }
-        tvResult.text = "${score}/${MainActivity.LEVEL_COUNT}"
+        tvMaxScore.text = "Max Score is ${preferences.getInt("max_score", 0)}"
     }
 }
